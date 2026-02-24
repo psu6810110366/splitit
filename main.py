df0fd96e5bb72f5e84b4fc4afff995536fb18c39
psu6810110498 <6810110498@psu.ac.th>
2026-02-24 22:28:55 +0700

    feat: setup screenmanager and initial views for all 4 screens

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+from kivy.core.window import Window
 
+# ตั้งค่าขนาดหน้าจอจำลอง (Mobile Size)
+Window.size = (360, 640)
+
+# สร้าง Class สำหรับแต่ละหน้าจอ (เดี๋ยวไปใส่รายละเอียดใน .kv)
+class HomeScreen(Screen):
+    pass
+
+class CreateBillScreen(Screen):
+    pass
+
+class SummaryScreen(Screen):
+    pass
+
+# ตัวจัดการหน้าจอ (ScreenManager)
+class WindowManager(ScreenManager):
+    pass
 
 class MainApp(App):
     def build(self):
-        # โหลดหน้าจอแรกขึ้นมาแสดงผล
-        return Label(text="Hello Kivy! Setup Successful.")
-
+        # โหลดไฟล์ UI
+        return Builder.load_file('main.kv')
 
 if __name__ == "__main__":
     MainApp().run()
