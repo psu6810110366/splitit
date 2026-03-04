0f965d4d2755b1f927987b3ffe9435737231851e
psu6810110498 <6810110498@psu.ac.th>
2026-03-04 15:36:35 +0700

    feat: implement summary screen UI to avoid blank page after next

--- screens/summary_screen.py
+++ screens/summary_screen.py
@@ -1,7 +1,12 @@
 from kivy.uix.screenmanager import Screen
 
 class SummaryScreen(Screen):
+    def go_back(self):
+        self.manager.current = 'new_split_screen'
+
     def on_save_and_finish(self):
         """Callback: บันทึกข้อมูลบิลลงระบบ (SQLite) และจบงาน"""
         print("Saving bill to database...")
-        # self.manager.current = 'result_screen' หรือ 'dashboard_screen'
+        # ในอนาคตต้องเอาข้อมูลจาก new_split_screen มาบันทึกเข้า DB
+        # สำหรับตอนนี้ ให้เด้งกลับไปหน้า Home ก่อน
+        self.manager.current = 'dashboard'
